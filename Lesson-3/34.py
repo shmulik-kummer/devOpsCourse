e458d58e418e71d1bf68b8e046fe45d57cbb5740
shmulik-kummer <kummersh@gmail.com>
2023-04-16 21:21:07 +0300

    Another commit

--- Lesson-3/34.py
+++ Lesson-3/34.py
@@ -3,10 +3,11 @@ import requests
 
 def check_if_url_up(url):
     response = requests.get(url)
-    if 200 <= response.status_code < 300:
+    if 200 <= response.status_code < 300:  # ddd
         return True
     else:
         return False
 
 
 print(check_if_url_up("https://api.github.com"))
+print("Hello")
